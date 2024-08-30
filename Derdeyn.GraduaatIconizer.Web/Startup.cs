eeecd7fd9536adb32b114845dd704c6d6d065ec7
sigged <sderdeyn@gmail.com>
2024-08-30 09:57:11 +0200

    Update to dotnet 8

--- Derdeyn.GraduaatIconizer.Web/Startup.cs
+++ Derdeyn.GraduaatIconizer.Web/Startup.cs
@@ -35,7 +35,10 @@ namespace Derdeyn.GraduaatIconizer.Web
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(mvcOptions =>
+            {
+                mvcOptions.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
